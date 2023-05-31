bdf4ec64906db645d239cd9ebdbaa15427a51070
RodrigoJr97 <rodrigojr1997@hotmail.com>
2023-05-31 16:28:29 -0300

    Logs Adicionado

--- src/main/java/com/veiculosmg/controller/CarroController.java
+++ src/main/java/com/veiculosmg/controller/CarroController.java
@@ -3,6 +3,7 @@ package com.veiculosmg.controller;
 import com.veiculosmg.model.entity.Carro;
 import com.veiculosmg.service.implementacao.ImplCarroService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/carro/v23")
+@Slf4j
 public class CarroController {
 
     private final ImplCarroService carroService;
@@ -22,61 +24,81 @@ public class CarroController {
 
     @GetMapping
     public ResponseEntity<List<Carro>> getCarros() {
+        log.info("Iniciando requisição para api/carro/v23");
         List<Carro> listaCarros = carroService.listaEntidades();
 
         if (listaCarros.isEmpty()) {
+            log.info("Requisição para api/carro/v23 concluída. Lista vazia.");
             return ResponseEntity.notFound().build();
         }
 
+        log.info("Requisição para api/carro/v23 concluída.");
         return ResponseEntity.ok(listaCarros);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> carro(@PathVariable Long id) {
+        log.info("Iniciando requisição para api/carro/v23/{}", id);
         Optional<Carro> carro = carroService.entidadePorId(id);
 
+        log.info("Requisição para api/carro/v23/{}", id + " concluída.");
         return new ResponseEntity<>(carro, HttpStatus.OK);
     }
 
     @GetMapping("/disponivel")
     public ResponseEntity<?> listaDisponivel() {
+        log.info("Iniciando requisição para api/carro/v23/disponivel");
         List<Carro> disponivel = carroService.disponivel();
 
         if (disponivel.isEmpty()) {
+            log.info("Requisição para api/carro/v23/disponivel concluída. Lista vazia.");
             return ResponseEntity.notFound().build();
         }
 
+        log.info("Requisição para api/carro/v23/disponivel concluída.");
         return ResponseEntity.ok(disponivel);
     }
 
     @GetMapping("/cat/{categoria}")
     public ResponseEntity<?> listaCategoria(@PathVariable String categoria) {
+        log.info("Iniciando requisição para api/carro/v23/cat/{}", categoria.toUpperCase());
         List<Carro> listaCategoria = carroService.listCategoria(categoria);
 
         if (listaCategoria.isEmpty()) {
+            log.info("Requisição para api/carro/v23/cat/{}", categoria.toUpperCase() + " concluída. Lista vazia.");
             return ResponseEntity.notFound().build();
         }
+
+        log.info("Requisição para api/carro/v23/cat/{}", categoria.toUpperCase() + " concluída.");
         return ResponseEntity.ok(listaCategoria);
     }
 
     @PostMapping
     public ResponseEntity<Carro> saveCarro(@Valid @RequestBody Carro carro) {
+        log.info("Iniciando requisição para api/carro/v23");
         Carro novoCarro = carroService.salvaNovaEntidade(carro);
+
+        log.info("Requisição para api/carro/v23 concluída.");
         return new ResponseEntity<>(novoCarro, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteCarro(@PathVariable Long id) {
+        log.info("Iniciando requisição para api/carro/v23/{}", id);
         Optional<Carro> carro = carroService.entidadePorId(id);
 
         return carro.map(car -> {
             carroService.deletaEntidade(id);
+            log.info("Requisição para api/carro/v23/{}", id + " concluída!");
             return ResponseEntity.noContent().build();
         }).orElseThrow();
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateCarro(@Valid @RequestBody Carro carro, @PathVariable Long id) {
+        log.info("Iniciando requisição para api/carro/v23/{}", id);
+        log.info("Body da requisição Carro:{}", carro);
+
         Optional<Carro> carroExiste = carroService.entidadePorId(id);
 
         return carroExiste.map(car -> {
--- src/main/java/com/veiculosmg/service/implementacao/ImplCarroService.java
+++ src/main/java/com/veiculosmg/service/implementacao/ImplCarroService.java
@@ -6,14 +6,17 @@ import com.veiculosmg.model.entity.Carro;
 import com.veiculosmg.model.repository.CarroRepository;
 import com.veiculosmg.service.CarroService;
 import com.veiculosmg.utilitarios.FormataNome;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
 @Service
+@Slf4j
 public class ImplCarroService implements CarroService {
 
     private final CarroRepository carroRepository;
@@ -24,22 +27,25 @@ public class ImplCarroService implements CarroService {
 
     @Override
     public List<Carro> listaEntidades() {
+        log.info("Iniciando busca da lista de carros.");
+
+        if (carroRepository.findAll().isEmpty()) {
+            log.info("Lista de Carros vazia.");
+            throw new RecursoNaoEncontradoException("Nenhum Carro Cadastrado!");
+        }
+
+        log.info("Busca de lista de carros concluída.");
         return carroRepository.findAll();
     }
 
     @Override
     public Optional<Carro> entidadePorId(Long id) {
-        Optional<Carro> carro = carroRepository.findById(id);
-
-        if (carro.isEmpty()) {
-            throw new RecursoNaoEncontradoException("Carro com ID: " + id + " Não Encontrado!");
-        }
-
-        return carro;
+        return Optional.ofNullable(verificaCarroExistente(id));
     }
 
     @Override
     public List<Carro> disponivel() {
+        log.info("Iniciando busca carros disponíveis.");
         return carroRepository.findAll()
                 .stream()
                 .filter(Carro::isDisponivel)
@@ -48,59 +54,85 @@ public class ImplCarroService implements CarroService {
 
     @Override
     public List<Carro> listCategoria(String categoria) {
+        log.info("Iniciando busca carros {}.", categoria.toUpperCase());
         return listaCategoria.apply(categoria);
     }
 
     @Override
     public Carro salvaNovaEntidade(Carro carro) {
+        log.info("Iniciando a criação de um novo Carro.");
         try {
+            log.info("Iniciando formatação do nome dos atributos.");
             Carro.formataAtributos(carro);
-            return carroRepository.save(carro);
+
+            carroRepository.save(carro);
+            log.info("Criação de novo Carro concluído.");
+            return carro;
         } catch (DataIntegrityViolationException ex) {
-            ex.printStackTrace();
-            throw new AtributoDuplicadoException("Placa informada já está cadastrada!");
+            log.error("Erro. Placa: {}", carro.getPlaca() + " já está cadastrada.", ex);
+            throw new AtributoDuplicadoException("Placa: " + carro.getPlaca() + " já está cadastrada!");
         }
+
     }
 
     @Override
     public void deletaEntidade(Long id) {
+        log.info("Iniciando o delete de um Carro.");
         Optional<Carro> existeCarroId = carroRepository.findById(id);
 
         if (existeCarroId.isEmpty()) {
+            log.warn("Carro com ID:{} ", id + " não encontrado!");
             throw new RecursoNaoEncontradoException("Carro com ID: " + id + " Não Encontrado!");
         }
 
+        log.info("Delete do Carro com ID:{} ", id + " concluída!");
         carroRepository.deleteById(id);
     }
 
     @Override
     public void updateEntidade(Carro carroAtualizado, Long id) {
-        Carro existeCarro = verificaCarroExistente(id);
+        log.info("Iniciando atualização do Carro ID: {}", id);
+        try {
+            Carro existeCarro = verificaCarroExistente(id);
 
-        verificarPlacasDiferentes(carroAtualizado, existeCarro);
+            log.info("Formatando nome dos atributos do Carro atualizado.");
+            Carro.formataAtributos(carroAtualizado);
 
-        carroAtualizado.setId(existeCarro.getId());
-        Carro.formataAtributos(carroAtualizado);
-        carroRepository.save(carroAtualizado);
+            verificarPlacasDiferentes(carroAtualizado, existeCarro);
+            carroAtualizado.setId(existeCarro.getId());
+
+            log.info("Atualização do carro concluída.");
+            carroRepository.save(carroAtualizado);
+        } catch (DataIntegrityViolationException ex) {
+            log.error("Erro ao atualizar o Carro.", ex);
+            throw new AtributoDuplicadoException("Placa informada já está cadastrada em outro veículo!");
+        }
     }
 
 
     /* Privado */
 
     private void verificarPlacasDiferentes(Carro carroAtualizado, Carro carroJaCadastrado) {
+        log.info("Iniciando a verificação se Placa: {}", carroAtualizado.getPlaca() + " já está cadastrada.");
         String placaDoCarro = carroJaCadastrado.getPlaca();
         String placaDoNovoCarro = carroAtualizado.getPlaca();
 
-        if (!placaDoCarro.equalsIgnoreCase(placaDoNovoCarro) && vericaSeExistePlacaCadastrada(carroAtualizado)) {
+        if (vericaSeExistePlacaCadastrada(carroAtualizado) && !placaDoCarro.equalsIgnoreCase(placaDoNovoCarro)) {
+            log.warn("Placa: {}", carroAtualizado.getPlaca() + " está cadastrada em outro veículo!");
             throw new AtributoDuplicadoException("Placa informada já está salva em outro veículo!");
         }
     }
 
     private Carro verificaCarroExistente(Long id) {
+        log.info("Verificando se o carro existe.");
         Optional<Carro> existeCarro = carroRepository.findById(id);
 
-        return existeCarro.orElseThrow(() ->
-                new RecursoNaoEncontradoException("Carro com ID: " + id + " Não Encontrado!"));
+        if (existeCarro.isEmpty()) {
+            log.info("Carro com ID:{}", id + " não encontrado!");
+            throw new RecursoNaoEncontradoException("Carro com ID: " + id + " Não Encontrado!");
+        }
+
+        return existeCarro.get();
     }
 
     private boolean vericaSeExistePlacaCadastrada(Carro carro) {
