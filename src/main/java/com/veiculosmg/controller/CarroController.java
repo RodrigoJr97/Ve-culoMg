47e16985b570aa960c60111e27581244b5ee7cd5
RodrigoJr97 <rodrigojr1997@hotmail.com>
2023-06-01 12:09:52 -0300

    Alteração nome das URIs e dos métodos no CarroController

--- src/main/java/com/veiculosmg/controller/CarroController.java
+++ src/main/java/com/veiculosmg/controller/CarroController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/carro/v23")
+@RequestMapping("api/carros")
 @Slf4j
 public class CarroController {
 
@@ -24,79 +24,79 @@ public class CarroController {
 
     @GetMapping
     public ResponseEntity<List<Carro>> getCarros() {
-        log.info("Iniciando requisição para api/carro/v23");
+        log.info("Iniciando requisição para api/carros");
         List<Carro> listaCarros = carroService.listaEntidades();
 
         if (listaCarros.isEmpty()) {
-            log.info("Requisição para api/carro/v23 concluída. Lista vazia.");
+            log.info("Requisição para api/carros concluída. Lista vazia.");
             return ResponseEntity.notFound().build();
         }
 
-        log.info("Requisição para api/carro/v23 concluída.");
+        log.info("Requisição para api/carros concluída.");
         return ResponseEntity.ok(listaCarros);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> carro(@PathVariable Long id) {
-        log.info("Iniciando requisição para api/carro/v23/{}", id);
+    public ResponseEntity<?> getCarroById(@PathVariable Long id) {
+        log.info("Iniciando requisição para api/carros/{}", id);
         Optional<Carro> carro = carroService.entidadePorId(id);
 
-        log.info("Requisição para api/carro/v23/{}", id + " concluída.");
+        log.info("Requisição para api/carros/{}", id + " concluída.");
         return new ResponseEntity<>(carro, HttpStatus.OK);
     }
 
-    @GetMapping("/disponivel")
-    public ResponseEntity<?> listaDisponivel() {
-        log.info("Iniciando requisição para api/carro/v23/disponivel");
+    @GetMapping("/disponiveis")
+    public ResponseEntity<?> getCarrosDisponiveis() {
+        log.info("Iniciando requisição para api/carros/disponiveis");
         List<Carro> disponivel = carroService.disponivel();
 
         if (disponivel.isEmpty()) {
-            log.info("Requisição para api/carro/v23/disponivel concluída. Lista vazia.");
+            log.info("Requisição para api/carros/disponiveis concluída. Lista vazia.");
             return ResponseEntity.notFound().build();
         }
 
-        log.info("Requisição para api/carro/v23/disponivel concluída.");
+        log.info("Requisição para api/carros/disponiveis concluída.");
         return ResponseEntity.ok(disponivel);
     }
 
-    @GetMapping("/cat/{categoria}")
-    public ResponseEntity<?> listaCategoria(@PathVariable String categoria) {
-        log.info("Iniciando requisição para api/carro/v23/cat/{}", categoria.toUpperCase());
+    @GetMapping("/categoria/{categoria}")
+    public ResponseEntity<?> getCarrosPorCategoria(@PathVariable String categoria) {
+        log.info("Iniciando requisição para api/carros/categoria/{}", categoria.toUpperCase());
         List<Carro> listaCategoria = carroService.listCategoria(categoria);
 
         if (listaCategoria.isEmpty()) {
-            log.info("Requisição para api/carro/v23/cat/{}", categoria.toUpperCase() + " concluída. Lista vazia.");
+            log.info("Requisição para api/carros/categoria/{}", categoria.toUpperCase() + " concluída. Lista vazia.");
             return ResponseEntity.notFound().build();
         }
 
-        log.info("Requisição para api/carro/v23/cat/{}", categoria.toUpperCase() + " concluída.");
+        log.info("Requisição para api/carros/categoria/{}", categoria.toUpperCase() + " concluída.");
         return ResponseEntity.ok(listaCategoria);
     }
 
     @PostMapping
-    public ResponseEntity<Carro> saveCarro(@Valid @RequestBody Carro carro) {
-        log.info("Iniciando requisição para api/carro/v23");
+    public ResponseEntity<Carro> createCarro(@Valid @RequestBody Carro carro) {
+        log.info("Iniciando requisição para api/carros");
         Carro novoCarro = carroService.salvaNovaEntidade(carro);
 
-        log.info("Requisição para api/carro/v23 concluída.");
+        log.info("Requisição para api/carros concluída.");
         return new ResponseEntity<>(novoCarro, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteCarro(@PathVariable Long id) {
-        log.info("Iniciando requisição para api/carro/v23/{}", id);
+    public ResponseEntity<?> deleteCarroById(@PathVariable Long id) {
+        log.info("Iniciando requisição para api/carros/{}", id);
         Optional<Carro> carro = carroService.entidadePorId(id);
 
         return carro.map(car -> {
             carroService.deletaEntidade(id);
-            log.info("Requisição para api/carro/v23/{}", id + " concluída!");
+            log.info("Requisição para api/carros/{}", id + " concluída!");
             return ResponseEntity.noContent().build();
         }).orElseThrow();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateCarro(@Valid @RequestBody Carro carro, @PathVariable Long id) {
-        log.info("Iniciando requisição para api/carro/v23/{}", id);
+    public ResponseEntity<?> updateCarroById(@Valid @RequestBody Carro carro, @PathVariable Long id) {
+        log.info("Iniciando requisição para api/carros/{}", id);
         log.info("Body da requisição Carro:{}", carro);
 
         Optional<Carro> carroExiste = carroService.entidadePorId(id);
